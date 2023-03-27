6d24149d4b87359df937757914756f8a1c5c5dc3
gfg7 <nakonechnaya040702@gmail.com>
2023-03-27 18:17:57 +0500

    add log to job

--- gRPCServer/Jobs/DropExpiredJob.cs
+++ gRPCServer/Jobs/DropExpiredJob.cs
@@ -5,16 +5,27 @@ namespace gRPCServer.Jobs
 {
     public class DropExpiredJob : IJob
     {
+        private readonly ILogger _logger;
         private readonly IBucketRemoval _service;
 
-        public DropExpiredJob(IBucketRemoval service)
+        public DropExpiredJob(IBucketRemoval service, ILogger<DropExpiredJob> logger)
         {
             _service = service;
+            _logger = logger;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
-            await _service.FlushExpired();
+            try
+            {
+                _logger.LogInformation($"Job {nameof(DropExpiredJob)} started");
+                await _service.FlushExpired();
+                _logger.LogInformation($"Job {nameof(DropExpiredJob)} finished");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Job {nameof(DropExpiredJob)} failed", ex);
+            }
         }
     }
 }
